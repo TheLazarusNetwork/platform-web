a94bad33257623a4636fba8c85221043888d14b8
Shruti Bansal <shrutibansal1802@gmail.com>
2021-06-24 16:52:12 +0530

    services ui

--- src/pages/Billing.js
+++ src/pages/Billing.js
@@ -1,20 +1,141 @@
+import React from "react";
+import Topnav from "../Components/Topnav";
+import { BasicTable } from "../utils/table";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import "./../styles/Dashboard/billing.css";
+import { RiMastercardFill } from "react-icons/ri";
+import { MdDelete } from "react-icons/md";
 
-import React from 'react'
-import Topnav from '../Components/Topnav';
+const currencies = [
+  {
+    value: "USD",
+    label: "$",
+  },
+  {
+    value: "EUR",
+    label: "€",
+  },
+  {
+    value: "BTC",
+    label: "฿",
+  },
+  {
+    value: "JPY",
+    label: "¥",
+  },
+];
 
 export default function Billing() {
-    return (
-        <>
-          {/* <SnackbarAlert
+  const [currency, setCurrency] = React.useState("EUR");
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
+  return (
+    <>
+      {/* <SnackbarAlert
             message={alertmsg}
             alertopen={alertopen}
             setAlertopen={setAlertopen}
             type={alerttype} // type = error, success, info ,warning
           /> */}
-          <div className="main">
-            <Topnav page="Billing" />
-            <div></div>
+      <div className="main">
+        <Topnav page="Billing" />
+        <div className="flex-div">
+          <div className="table  mid-details-box">
+            <div className="table-top">
+              <div className="title"> Transaction History</div>
+              <TextField
+                id="standard-select-currency"
+                select
+                value={currency}
+                onChange={handleChange}
+                helperText="Please select your currency"
+              >
+                {currencies.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </div>
+            <div className="divider"></div>
+            <div className="mid-details-box shadow">
+              <div className="filters">
+                <TextField
+                  id="standard-select-currency"
+                  select
+                  value={currency}
+                  onChange={handleChange}
+                  helperText="Time"
+                >
+                  {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>{" "}
+                <TextField
+                  id="standard-select-currency"
+                  select
+                  value={currency}
+                  onChange={handleChange}
+                  helperText="Service "
+                >
+                  {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </div>
+              <BasicTable />
+            </div>
           </div>
-        </>
-      );
+          <div className="mid-details-box ">
+            <div className="title">My Cards</div>
+            <div>
+              <button className="grey-btn">Add new Card</button>
+              <button className="grey-btn">Create Card</button>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <div>Card Holder's name</div>
+                <icon className="card-icon">
+                  <RiMastercardFill />
+                </icon>
+                <icon className="card-delete">
+                  <MdDelete />
+                </icon>
+                <p> xxxx-xxxx-xxxx-1234</p>
+                
+              </div>
+              <div className="card">
+                <div>Card Holder's name</div>
+                <icon className="card-icon">
+                  <RiMastercardFill />
+                </icon>
+                <icon className="card-delete">
+                  <MdDelete />
+                </icon>
+                <p> xxxx-xxxx-xxxx-1234</p>
+                
+              </div>
+              <div className="card">
+                <div>Card Holder's name</div>
+                <icon className="card-icon">
+                  <RiMastercardFill />
+                </icon>
+                <icon className="card-delete">
+                  <MdDelete />
+                </icon>
+                <p> xxxx-xxxx-xxxx-1234</p>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/services/AnonymousVPN.js
+++ src/pages/services/AnonymousVPN.js
@@ -1,19 +1,66 @@
-import React from 'react'
-import Topnav from '../../Components/Topnav';
+import React from "react";
+import Topnav from "../../Components/Topnav";
+import "./../../styles/Dashboard/billing.css";
 
 export default function AnonymousVPN() {
-    return (
-        <>
-          {/* <SnackbarAlert
+  return (
+    <>
+      {/* <SnackbarAlert
             message={alertmsg}
             alertopen={alertopen}
             setAlertopen={setAlertopen}
             type={alerttype} // type = error, success, info ,warning
           /> */}
-          <div className="main">
-            <Topnav page="Anonymous VPN" />
-            <div></div>
+      <div className="main">
+        <Topnav page="Anonymous VPN" />
+        <div className="details-box shadow">
+          <div className="box-title">About </div>
+          <ul className="service-details">
+            <li>sdkjfldsd alsdkfjlask laskdjfsladk lkajsdfd lkasdjflksdj</li>
+            <li>sdkjfldsd alsdkfjlaskd lkasdjflksdj</li>
+            <li>sdkjfldsd alsdkfjlaskd lkasdjflksdj</li>
+            <li>sdkjfldsd alsdkfjlaskd lkasdjflksdj</li>
+          </ul>
+        </div>
+
+        <div className="pricing details-box shadow">
+          <div className="box-title"> Pricing options</div>
+          <div className="flex-div">
+            <div className="pricing-div">
+              <div className="title center">Basic</div>
+              <div className="divider" />
+              <ul className="">
+                <li>5 users</li>
+                <li>10 hours weekly</li>
+              </ul>
+              <div className="divider" />
+              <div className="price"> $10</div>
+            </div>
+            <div className="pricing-div">
+              <div className="title center">Basic</div>
+              <div className="divider" />
+              <ul className="">
+                <li>5 users</li>
+                <li>10 hours weekly</li>
+              </ul>
+              <div className="divider" />
+              <div className="price"> $10</div>
+            </div>           <div className="pricing-div">
+              <div className="title center">Basic</div>
+              <div className="divider" />
+              <ul className="">
+                <li>5 users</li>
+                <li>10 hours weekly</li>
+              </ul>
+              <div className="divider" />
+              <div className="price"> $10</div>
+            </div>
           </div>
-        </>
-      );
+          <div>
+          <button className='save-btn'> Activate Service</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
